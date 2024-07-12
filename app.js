533add290efddc00254de457e3904c1d9da2be08
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-07-12 19:01:23 +0200

    Inicio de sesion 2.0 y registro

--- app.js
+++ app.js
@@ -19,32 +19,51 @@ const baseDeDatos = [
     }
 ]
 
+//Dejar estas variables fuera de la funcion y luega a la funcion apregarle parametros, hace que la funcion sea mas flexible y reutilizable.
 
 
-function inicioSesion() {
-    const mail = prompt('Ingresa un mail');
-    
-    // Buscar el email en la base de datos // usuario es donde se aloja el resultado de la comparacion
+
+
+function inicioSesion() {  
+    const mail = prompt('Ingresa un mail');  
+    // Buscar el email en la base de datos // corroborarMail es donde se aloja el resultado de la comparacion
     const corroborarMail = baseDeDatos.find(baseDeDatos => baseDeDatos.email === mail);
 
     if (corroborarMail) {
-        alert('Mail correcto');
+        alert('mail ok');
     } else {
-        alert('Mail incorrecto');
-    }
-
+        alert('Mail incorrecto'); 
+            prompt('Ingresa un mail'); 
+            return inicioSesion(mail, contraseña);  
+    }  
     const contraseña = prompt ('Ingresa la contraseña');
-
 //Buscar la constraseña en la base de datos. Creo una variable para alojar el resultado de la comparacion
     const corroborarContraseña = baseDeDatos.find(baseDeDatos => baseDeDatos.password === contraseña);
-
     if (corroborarContraseña) {
         alert('Bienvenido');
     } else {
         alert('contraseña incorrecta');
+            prompt ('Ingresa la contraseña');       
     }
 }
+//inicioSesion();
+
+function registro () {
+    const mail = prompt('Ingresa un mail');  
+    // Buscar el email en la base de datos // corroborarMail es donde se aloja el resultado de la comparacion
+    const corroborarMail = baseDeDatos.find(baseDeDatos => baseDeDatos.email === mail);
+
+    if (corroborarMail) {
+        alert('Su mail ya esta regitrado');
+    } else {
+         alert ('Su mail se ha registrado con éxito');        
+}
+    const contraseña = prompt ('Ingresa una contraseña');
+
+    alert ("Inicie sesion")
 
 
+baseDeDatos.push (mail, contraseña);
+}
 
-inicioSesion();
+registro ();
